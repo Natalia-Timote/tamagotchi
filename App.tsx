64e07ebaf06c7472a0622e9ff1a2721b5b1e5699
Natalia-Timote <nataliamirian.nt@gmail.com>
2025-11-14 23:37:32 -0300

    Criando StudygotchiContext

--- App.tsx
+++ App.tsx
@@ -4,18 +4,21 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import Home from './src/pages/Home/Home';
 import Layout from './src/pages/Layout/Layout';
 import Game from './src/pages/Game/Game';
+import StudygotchiProvider from './src/Context/StudygotchiContext';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='/studygotchi' element={<Game />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <StudygotchiProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='/studygotchi' element={<Game />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </StudygotchiProvider>
   )
 }
 
---
+++ src/Context/StudygotchiContext.tsx
@@ -0,0 +1,21 @@
+import { createContext, useState, type ReactNode } from "react";
+
+interface StudygotchiContextType {
+    characterName: string,
+    setCharacterName: React.Dispatch<React.SetStateAction<string>>,
+    characterSelect: string,
+    setCharacterSelect: React.Dispatch<React.SetStateAction<string>>
+}
+
+const StudygotchiContext = createContext<StudygotchiContextType | null>(null);
+
+export default function StudygotchiProvider({ children }: { children: ReactNode }) {
+    const [characterName, setCharacterName] = useState("");
+    const [characterSelect, setCharacterSelect] = useState("");
+
+    return (
+        <StudygotchiContext.Provider value={{ characterName, setCharacterName, characterSelect, setCharacterSelect }}>
+            {children}
+        </StudygotchiContext.Provider>
+    )
+}
