cccaee5b19f1dfa461c6cdf91fe020366cfae7b8
Natalia-Timote <nataliamirian.nt@gmail.com>
2025-11-21 19:54:39 -0300

    Corrigindo tipo

--- src/components/CalendarStudy/index.tsx
+++ src/components/CalendarStudy/index.tsx
@@ -2,13 +2,17 @@ import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
 import './CalendarStudy.css';
 
-export default function CalendarStudy({ studyingDays }) {
+interface CalendarStudyProps {
+    studyingDays: string[]
+}
+
+export default function CalendarStudy({ studyingDays }: CalendarStudyProps) {
     return (
-        <Calendar 
+        <Calendar
             className="calendar"
-            tileClassName={({ date}) => 
-                studyingDays.includes(date.toISOString().split("T") [0]) ? 'studied-day' : null
-            } 
+            tileClassName={({ date }) =>
+                studyingDays.includes(date.toISOString().split("T")[0]) ? 'studied-day' : null
+            }
         ></Calendar>
     )
 }
